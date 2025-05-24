2b7a642be9ed6494bbb90352dfd13728de693fb2
Designer Tawhid <93260879+designertawhid@users.noreply.github.com>
2025-05-24 16:29:59 +0600

    Implement Splash Screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { useFrameworkReady } from '@/hooks/useFrameworkReady';
@@ -15,8 +15,15 @@ import * as SplashScreen from 'expo-splash-screen';
 // Keep the splash screen visible while we fetch resources
 SplashScreen.preventAutoHideAsync();
 
+// Set the animation options. This is optional.
+SplashScreen.setOptions({
+  duration: 400,
+  fade: true,
+});
+
 export default function RootLayout() {
   useFrameworkReady();
+  const [appIsReady, setAppIsReady] = useState(false);
 
   const [fontsLoaded, fontError] = useFonts({
     'Inter-Regular': Inter_400Regular,
@@ -26,20 +33,40 @@ export default function RootLayout() {
   });
 
   useEffect(() => {
-    if (fontsLoaded || fontError) {
-      // Hide splash screen once fonts are loaded or if there's an error
-      SplashScreen.hideAsync();
+    async function prepare() {
+      try {
+        // Pre-load fonts, make any API calls you need to do here
+        await Promise.all([
+          // Add any additional async operations here
+        ]);
+        
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Remove this if you copy and paste the code!
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
     }
-  }, [fontsLoaded, fontError]);
 
-  // Wait for fonts to load
-  if (!fontsLoaded && !fontError) {
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady && (fontsLoaded || fontError)) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady, fontsLoaded, fontError]);
+
+  // Wait for fonts to load and app to be ready
+  if (!appIsReady || (!fontsLoaded && !fontError)) {
     return null;
   }
 
   return (
     <ThemeProvider>
-      <Stack screenOptions={{ headerShown: false }}>
+      <Stack screenOptions={{ headerShown: false }} onLayout={onLayoutRootView}>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" options={{ title: 'Not Found' }} />
       </Stack>
